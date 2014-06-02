a11e275cdbbddcd1b515a11d4f40f2139bb73c37
Alexander Ivanov <ivanov@fidor.de>
2014-06-02 18:34:44 +0300

    Release is 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='matchete',
-      version='0.0.1',
+      version='0.0.2',
       description='Python simple pattern-matching based method overloading',
       author='Alexander Ivanov',
       author_email='alehander42@gmail.com',
