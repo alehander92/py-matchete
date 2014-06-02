f01fe000539d22f3542fdea60482619b6a413083
Alexander Ivanov <ivanov@fidor.de>
2014-06-02 17:40:24 +0300

    Fix whitespace

--- matchete/on.py
+++ matchete/on.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def on(*guards):
     def decorator(func):
         mod = sys.modules[func.__module__]
@@ -11,7 +12,8 @@ def on(*guards):
         return call_overload(func.__name__)
     return decorator
 
-def call_overload(name):    
+
+def call_overload(name):
     def match_guard(guard, arg):
         print(guard, arg)
         if isinstance(guard, str) and len(guard) > 0:
@@ -25,7 +27,7 @@ def call_overload(name):
             print arg, guard, isinstance(arg, guard)
             return isinstance(arg, guard)
         elif callable(guard):
-            return guard(arg)         
+            return guard(arg)
         else:
             return guard == arg
 
@@ -41,6 +43,7 @@ def call_overload(name):
 
     return wrapper
 
+
 def extract_expected(obj, attribute):
     if attribute[0] == '.':
         if hasattr(obj, attribute[1:]):
@@ -48,27 +51,31 @@ def extract_expected(obj, attribute):
     elif attribute[0] == '#':
         if hasattr(obj, attribute[1:]) and callable(obj, attribute[1:]):
             return getattr(obj, attribute[1:])()
-        
-    
+
+
 def eq(attribute, value):
     def wrapper(arg):
         return value == extract_expected(arg, attribute)
     return wrapper
+
 
 def is_in(attribute, collection):
     def wrapper(arg):
         return extract_expected(arg, attribute) in collection
     return wrapper
 
+
 def not_eq(attribute, value):
     def wrapper(arg):
         return value != extract_expected(arg, attribute)
     return wrapper
 
+
 def contains(collection, value):
     def wrapper(arg):
         return any(select(lambda a: a == value, map(lambda a: extract_expected(arg, a), collection)))
     return wrapper
+
 
 def Any(arg):
     return True
